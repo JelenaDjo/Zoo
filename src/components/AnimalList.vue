34ba6c201e5329c47e36a18899a74a209ca1a389
JelenaDJo <djokicj@yahoo.com>
2018-11-20 20:58:34 +0100

    Sesti zadatak

--- src/components/AnimalList.vue
+++ src/components/AnimalList.vue
@@ -10,6 +10,12 @@
 
             <label>Date Of Birth</label>
             <input v-model="newAnimal.date" placeholder="Date Of Birth"><br/>
+
+            <select v-model="newAnimal.sector">
+                <option disabled value="">Please choose one</option>
+                <option v-for="(sector, index) in sectors" :key="index" :value="sector">{{sector.name}}</option>
+            </select>
+
             <button type="submit">Add Animal</button>
         </form>
 
@@ -20,6 +26,7 @@
                 <th> Ime</th>
                 <th>Vrsta</th>
                 <th>Datum rodjenja</th>
+                <th>Sector</th>
             </thead>
             <tbody>
                 <tr v-for="(animal, index) in animals" :key="index">
@@ -27,6 +34,7 @@
                 <td>{{animal.name}}</td>
                 <td>{{animal.species}}</td>
                 <td>{{animal.date ? animal.date: 'Nepostoji'}}</td>
+                <td>{{animal.sector.name}}</td>
                 <td>
                     <button @click="removeAnimal(animal)" type="submit"> Remove </button>
                 </td>
@@ -42,21 +50,28 @@
 
 
 <script>
+
+const sectors = [
+    {name: 'Home animals', surface: 'Home'},
+    {name: 'Air animals', surface: 'Air'},
+    {name: 'Water animals', surface: 'Water'}
+
+
+]
 export default {
     data(){
         return { 
-        newAnimal: {
-            species: '',
-            name: '',
-            date: ''
-            },
+        sectors: sectors,
+        
+        newAnimal: { },
+
 
         animals: [
-            {name: 'Cicko', species: 'Pas', date: '20/10/2015'},
-            {name: 'Hulk', species: 'Zec'},
-            {name: 'Barni', species: 'Hrcak', date: '12/08/2009'},
-            {name: 'Fredy', species: 'Guster'},
-            {name: 'Beba', species: 'Macka', date: '18/05/2013'}
+            {name: 'Cicko', species: 'Pas', date: '20/10/2015', sector:sectors[0]},
+            {name: 'Hulk', species: 'Zec', sector:sectors[0]},
+            {name: 'Barni', species: 'Papagaj', date: '12/08/2009', sector:sectors[1]},
+            {name: 'Fredy', species: 'Delfin', sector:sectors[2]},
+            {name: 'Beba', species: 'Albatros', date: '18/05/2013', sector:sectors[1]}
 
         ]
         }
